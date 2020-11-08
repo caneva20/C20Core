f308e072d2acb0067614429c3f23abca2526b75f
Fabricio Caneva <hardlymersk@gmail.com>
2020-11-08 18:37:48 -0300

    initial

---
+++ src/main/java/me/caneva20/c20core/Core.java
@@ -0,0 +1,10 @@
+package me.caneva20.c20core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Core extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("[C20 Core] Loaded!");
+    }
+}
