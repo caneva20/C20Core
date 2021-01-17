eada6ab90d0bd8638713ed589b46ac3312cdb705
Fabricio Caneva <hardlymersk@gmail.com>
2021-01-17 19:47:25 -0300

    fix NoSuchMethodError

--- src/main/java/me/caneva20/c20core/Core.java
+++ src/main/java/me/caneva20/c20core/Core.java
@@ -3,19 +3,16 @@ package me.caneva20.c20core;
 import io.papermc.lib.PaperLib;
 import me.caneva20.c20core.modules.messagedispatcher.MessageDispatcher;
 import me.caneva20.messagedispatcher.Messaging;
-import me.caneva20.messagedispatcher.dispachers.IConsoleMessageDispatcher;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Core extends JavaPlugin {
-    private IConsoleMessageDispatcher dispatcher;
-
     @Override
     public void onEnable() {
         PaperLib.suggestPaper(this);
 
         MessageDispatcher.configure(this);
 
-        dispatcher = Messaging.createConsoleDispatcher(this, true);
+        var dispatcher = Messaging.createConsoleDispatcher(this);
 
         dispatcher.success("Enabled & all  \\\\o/");
         dispatcher.success("dependencies    |");
